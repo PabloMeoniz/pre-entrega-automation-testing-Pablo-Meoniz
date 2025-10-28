b3b20bf4a05975f78b46fabdd592cd935fe34556
Pablo Meoniz <meoniz.p.o@gmail.com>
2025-10-28 10:22:21 -0300

    subida de primera prueba con el login con selenium

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,14 +1,24 @@
 import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # Agrega el directorio padre al path
+from utils.helpers import login_sauce_demo
+from utils.helpers import get_driver
 @pytest.fixture
 def driver():
     #configurcion para consultar a selenium web driver
+    driver = get_driver()
+    yield driver
+    driver.quit()
 
 
-
-def test_login():
+def test_login(driver):
+    login_sauce_demo(driver)
     #logueo de usuario con username y password
     #click al boton de login
-    #redirigir a la pagina de inventario
+    #redirigir a la pagina de inventario (ventanita principal)
+
+'''
 def test_catalogo():
     #logeo de usuario con username y password
     #click al boton de login
@@ -21,3 +31,5 @@ def test_carrito():
     #llevarme a la pagina de carrito de compra
     #incremento de carrito al agregar un producto al carrito
     #comprobar que en el carrito aparezca el producto correcto
+
+'''
