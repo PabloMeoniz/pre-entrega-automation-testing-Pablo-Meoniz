282d2df6bd8aa31598acad79e4f9af370ecd9c40
Pablo Meoniz <meoniz.p.o@gmail.com>
2025-10-27 22:10:01 -0300

    Subida inicial de los archivos de la pre-entrega

---
+++ tests/test_saucedemo.py
@@ -0,0 +1,23 @@
+import pytest
+@pytest.fixture
+def driver():
+    #configurcion para consultar a selenium web driver
+
+
+
+def test_login():
+    #logueo de usuario con username y password
+    #click al boton de login
+    #redirigir a la pagina de inventario
+def test_catalogo():
+    #logeo de usuario con username y password
+    #click al boton de login
+    #podamos verificar el titulo pero del html
+    #comprobar si existen productos en la pagina visibles (len()>0)
+    #verificar elementos importantes de la pagina
+def test_carrito():
+    #logeo de usuario con username y password
+    #click al boton de login
+    #llevarme a la pagina de carrito de compra
+    #incremento de carrito al agregar un producto al carrito
+    #comprobar que en el carrito aparezca el producto correcto
