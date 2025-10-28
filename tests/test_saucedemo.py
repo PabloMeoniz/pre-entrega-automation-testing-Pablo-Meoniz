0c20c8e65601c8f251498c4693cb57b562d8aee6
Pablo Meoniz <meoniz.p.o@gmail.com>
2025-10-28 12:00:40 -0300

    agregado de test de carrito y capturas

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -12,49 +12,45 @@ def driver():
     yield driver
     driver.quit()
 
-"""
+
 def test_login(driver):
     login_sauce_demo(driver) # evalua la funcion de login si funciona correctamente
     assert "/inventory.html" in driver.current_url #evalua si esta en la pagina de inventario despues del login
     titulo = driver.find_element(By.CSS_SELECTOR, "div.header_secondary_container .title").text #busca el titulo de la pagina
     assert titulo == 'Products' #verifica que el titulo sea PRODUCTS
-"""
+    driver.save_screenshot("evidenia_login_correcto.png")
 
-"""
+
+
+
 def test_catalogo(driver):
     login_sauce_demo(driver)
 
     products = driver.find_elements(By.CLASS_NAME, 'inventory_item')                #busca los productos en la pagina
     products = driver.find_elements(By.CLASS_NAME, 'inventory_list')                # busca los productos en la pagina
     assert len(products) > 0                                                        #verifica que haya productos en la pagina
-    
+    driver.save_screenshot("evidencia_existencia_productos.png")
                                                                                     #logeo de usuario con username y password
                                                                                     #click al boton de login
                                                                                     #podamos verificar el titulo pero del html
                                                                                     #comprobar si existen productos en la pagina visibles (len()>0)
                                                                                     #verificar elementos importantes de la pagina
-"""   
+   
 
 
 def test_carrito(driver):
     login_sauce_demo(driver)
 
-    products = driver.find_elements(By.CLASS_NAME, 'inventory_list')                #busca los productos en la pagina
-    total_products = len(products)                                              # cuenta la cantidad de productos encontrados       
-    products[0].find_element(By.TAG_NAME, 'button').click()                          # hace click en el boton del primer producto para agregar al carrito
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')                #busca los productos en la pagina
+    total_products = len(products)                                                  # cuenta la cantidad de productos encontrados       
+    
+    if total_products >= 2:
+        products[0].find_element(By.TAG_NAME, 'button').click()                     # hace click en el boton del primer producto para agregar al carrito
+        products[1].find_element(By.TAG_NAME, 'button').click()
 
-    badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
-    assert badge ==       #busca el elemento del carrito
-     #logueo de usuario con username y password
+        badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+        assert badge ==  "2"
+    driver.save_screenshot("evidencia_productos_carrito.png")                                                         #busca el elemento del carrito
+    #logueo de usuario con username y password
     #click al boton de login
     #redirigir a la pagina de inventario (ventanita principal)
-
-'''
-def test_carrito():
-    #logeo de usuario con username y password
-    #click al boton de login
-    #llevarme a la pagina de carrito de compra
-    #incremento de carrito al agregar un producto al carrito
-    #comprobar que en el carrito aparezca el producto correcto
-
-'''
